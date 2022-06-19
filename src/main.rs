f03f51196a26a02f24ded2ce1172a95f6b063829
admodev <admodevcodes@outlook.com>
2022-06-19 00:00:48 -0300

    change resolution, add camera

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode};
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
-pub const RESOLUTION: f32 = 128.0 / 72.0;
+pub const RESOLUTION: f32 = 80.0 / 60.0;
 
 fn main() {
-    let height: f32 = 720.0;
+    let height: f32 = 600.0;
 
     App::new()
         .insert_resource(ClearColor(CLEAR))
@@ -16,6 +16,21 @@ fn main() {
             resizable: false,
             ..Default::default()
         })
+        .add_startup_system(spawn_camera)
         .add_plugins(DefaultPlugins)
         .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera: OrthographicCameraBundle = OrthographicCameraBundle::new_2d();
+
+    camera.orthographic_projection.top = 1.0;
+    camera.orthographic_projection.bottom = -1.0;
+
+    camera.orthographic_projection.right = -1.0;
+    camera.orthographic_projection.left = -1.0;
+
+    camera.orthographic_projection.scaling_mode = ScalingMode::None;
+
+    commands.spawn_bundle(camera);
 }
