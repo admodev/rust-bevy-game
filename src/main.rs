95207e217bcad9864b29e526b0352585ab05eab5
admodev <admodevcodes@outlook.com>
2022-06-18 23:55:53 -0300

    add resolution constants

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use bevy::prelude::*;
 
 pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 128.0 / 72.0;
 
 fn main() {
+    let height: f32 = 720.0;
+
     App::new()
         .insert_resource(ClearColor(CLEAR))
         .insert_resource(WindowDescriptor {
-            width: 1280.0,
-            height: 720.0,
+            width: height * RESOLUTION,
+            height: height,
             title: "Bevy Tutorial".to_string(),
             vsync: true,
             resizable: false,
