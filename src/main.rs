6e9317cc5dda8fb427c4ed566bff09d54b623aef
admodev <admodevcodes@outlook.com>
2022-06-18 23:50:11 -0300

    init project

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use bevy::prelude::*;
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: 1280.0,
+            height: 720.0,
+            title: "Bevy Tutorial".to_string(),
+            vsync: true,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
+}
